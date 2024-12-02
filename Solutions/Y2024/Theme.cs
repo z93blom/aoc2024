7c82b52a546f31f8f743a6876e89386ff41a5e46
Fredrik Blom <fredrik@familjenblom.se>
2024-12-02 10:25:01 +0100

    Finally solved part 2 of todays problem.

--- Solutions/Y2024/Theme.cs
+++ Solutions/Y2024/Theme.cs
@@ -4,7 +4,11 @@ class Theme : ITheme
 {
     public Dictionary<string, int> Override(Dictionary<string, int> themeColors)
     {
+        themeColors["calendar-color-3g"] = 0x00cc00;
         themeColors["calendar-color-3s"] = 0xe3b585;
+        themeColors["calendar-color-3y"] = 0xffff66;
+        themeColors["calendar-color-6b"] = 0x009900;
+        themeColors["calendar-color-6o"] = 0xff9900;
         themeColors["calendar-color-6y"] = 0xffff66;
         themeColors["calendar-color-8n"] = 0x886655;
         return themeColors;
