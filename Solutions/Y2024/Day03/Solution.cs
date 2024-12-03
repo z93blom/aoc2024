9e15643a7be3c8e7abbba433e5d4fcb52605d838
Fredrik Blom <fredrik@familjenblom.se>
2024-12-03 08:13:14 +0100

    Day 03 solved

---
+++ Solutions/Y2024/Day03/Solution.cs
@@ -0,0 +1,72 @@
+using AdventOfCode.Framework;
+using AdventOfCode.Utilities;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions.Y2024.Day03;
+
+[RegisterKeyedTransient("2024-03")] partial class Solution { }
+[RegisterTransient()] partial class Solution { }
+
+partial class Solution : ISolver
+{
+
+    //[GeneratedRegex(@"((mul)\(\d{1,3},\d{1,3}\))|(do\(\))|(don\'t\(\))")]
+    //private static partial Regex MulExpressions();
+    
+    public int Year => 2024;
+    public int Day => 3;
+    public string GetName() => "Mull It Over";
+
+    public IEnumerable<object> Solve(string input, Func<TextWriter> getOutputFunction)
+    {
+        // var emptyOutput = () => new NullTextWriter();
+        yield return PartOne(input, getOutputFunction);
+        yield return PartTwo(input, getOutputFunction);
+    }
+
+    static object PartOne(string input, Func<TextWriter> getOutputFunction)
+    {
+        var result = input.Matches(@"mul\((\d{1,3}),(\d{1,3})\)")
+            .Select(m => int.Parse(m.Groups[1].ValueSpan) * int.Parse(m.Groups[2].ValueSpan))
+            .Sum();
+        return result;
+    }
+
+    static object PartTwo(string input, Func<TextWriter> getOutputFunction)
+    {
+        var ops = input.Matches(@"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)")
+            .Select(GetOp)
+            .ToList();
+
+        var result = ops.Aggregate((true, 0), (t, op) =>
+            {
+                var (enabled, sum) = t;
+                var result = (op, enabled) switch
+                {
+                    (Enable, _) => (true, sum),
+                    (Disable, _) => (false, sum),
+                    (Multiply m, true) => (true, sum + m.Val),
+                    (Multiply m, false) => (false, sum),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                return result;
+            });
+        return result.Item2;
+
+        Op GetOp(Match m)
+        {
+            if (m.Value.StartsWith("mul")) return new Multiply(int.Parse(m.Groups[1].ValueSpan) * int.Parse(m.Groups[2].ValueSpan));
+            if (m.Value.StartsWith("don't")) return new Disable();
+            if (m.Value.StartsWith("do")) return new Enable();
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private class Op();
+    private class Enable() : Op;
+    private class Disable() : Op;
+    private class Multiply(int val) : Op
+    {
+        public int Val { get; } = val;
+    }
+}
--- Solutions/Y2024/Theme.cs
+++ Solutions/Y2024/Theme.cs
@@ -4,13 +4,20 @@ class Theme : ITheme
 {
     public Dictionary<string, int> Override(Dictionary<string, int> themeColors)
     {
+        themeColors["calendar-color-3b"] = 0x5555bb;
         themeColors["calendar-color-3g"] = 0x00cc00;
         themeColors["calendar-color-3s"] = 0xe3b585;
         themeColors["calendar-color-3y"] = 0xffff66;
         themeColors["calendar-color-6b"] = 0x009900;
         themeColors["calendar-color-6o"] = 0xff9900;
+        themeColors["calendar-color-6r"] = 0xff0000;
+        themeColors["calendar-color-6u"] = 0x0066ff;
         themeColors["calendar-color-6y"] = 0xffff66;
+        themeColors["calendar-color-8e"] = 0xcccccc;
+        themeColors["calendar-color-8i"] = 0xff0000;
         themeColors["calendar-color-8n"] = 0x886655;
+        themeColors["calendar-color-8w"] = 0xcccccc;
+
         return themeColors;
     }
 }
